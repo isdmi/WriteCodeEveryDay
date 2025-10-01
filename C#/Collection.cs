352b9473ccbe53961c1e58f0a845bdc60a9e65b1
MitsuhitoIshida <isdmi.421@gmail.com>
2025-10-01 22:17:31 +0900

    コレクション式、Spanについてを追加

---
+++ C#/Collection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Collection
+    {
+        public void Test() 
+        {
+            // コレクション式について
+
+            // 今まではこう書いてた。
+            List<int> oldList = new List<int>() { 1, 2, 3 };
+
+            // C#12からはこう書くことができる。
+            List<int> newList = [1, 2, 3];
+            List<string> hogeList = ["AAA", "BBB", "CCC"];
+
+            // Spanについて　C#7
+            // 配列から一部だけ抜き出すようなものと解釈・・・
+            // IEnumerableではないのでLINQは使えない。
+            string[] stringArray = ["1", "2", "3", "4", "5"];
+            Span<String> stringSpan = stringArray.AsSpan(0, 3);
+
+            foreach (string Fuga in stringSpan) 
+            {
+                Debug.WriteLine(Fuga); // この結果は1,2,3になる。
+            }
+        }
+    }
+}
--- C#/TestClass.cs
+++ C#/TestClass.cs
@@ -1,21 +1,23 @@
+ï»¿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Test
 {
-    public partial class Form1 : Form
+    internal class TestClass
     {
-        public Form1()
+        public void Test() 
         {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // Count By‚ÌŽÀ‘•
+            // Count Byã®å®Ÿè£…
             int[] randomDiceNumbers = [1, 2, 2, 4, 2, 6, 1, 5, 3, 2, 1, 3, 4, 5, 6, 3, 1, 1, 3, 4, 2, 6, 5, 4, 1, 3, 4, 1, 3, 6, 4, 2, 1];
 
             var res = randomDiceNumbers.CountBy(x => x).ToList();
             res.ForEach(x => Debug.WriteLine(x));
+
         }
     }
 }
