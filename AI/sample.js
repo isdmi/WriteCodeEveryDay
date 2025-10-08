37f8d5cc29bc64cb9413dbeed0d2943f75b1eeb7
MitsuhitoIshida <isdmi.421@gmail.com>
2025-10-08 23:24:52 +0900

    生成AIを使ったWebシステムの構築

---
+++ AI/sample.js
@@ -0,0 +1,16 @@
+import OpenAI from 'openai';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+const response = await client.responses.create({
+  model: 'gpt-4o',
+  instructions: 'You are a coding assistant that talks like a pirate',
+  input: 'Are semicolons optional in JavaScript?',
+});
+
+console.log(response.output_text);
---
+++ AI/server.js
@@ -0,0 +1,25 @@
+import http from 'http';
+import OpenAI from 'openai';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = new http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write(response.output_text);
+    res.end();
+});
+
+const client = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+const response = await client.responses.create({
+  model: 'gpt-4o',
+  instructions: '日本語で答えてください',
+  input: '一分間スピーチの内容を考えてください',
+});
+
+const port = 8080;
+server.listen(port);
+console.log('Server listen on port ' + port);
