c1e0127f8f9e4445450e4d5010a136eb9950340a
MitsuhitoIshida <isdmi.421@gmail.com>
2026-02-15 18:40:41 +0900

    AtCoder0215

---
+++ C#/Console/ConsoleApp1/AtCoder0215.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AtCoder0215
+    {
+        public void QuestionA()
+        {
+            var S1 = Console.ReadLine();
+            char[] N = S1.ToCharArray();
+
+            if (N[0] == N[N.Count()-1])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        public void QuestionB()
+        {
+            var S1 = Console.ReadLine();
+            int N = int.Parse(S1);
+
+            var array1 = new List<string>();
+
+            for (int i = 0; i < N; i++) 
+            {
+                var S2 = Console.ReadLine();
+                array1.Add(S2);
+            }
+
+            var maxLength = array1.Select(x => x.Length).Max();
+
+            foreach (var item in array1) 
+            {
+                if (item.Length == maxLength) 
+                {
+                    Console.WriteLine(item);
+                    continue;
+                }
+
+                var addLength = (maxLength - item.Length) / 2;
+                string appendStr = new string('.', addLength);
+                Console.WriteLine(appendStr + item + appendStr);                
+            }
+        }
+    }
+}
--- C#/Console/ConsoleApp1/Program.cs
+++ C#/Console/ConsoleApp1/Program.cs
@@ -1,22 +1,4 @@
 ﻿using ConsoleApp1;
 
-AtCoder0208 test = new AtCoder0208();
+AtCoder0215 test = new AtCoder0215();
 test.QuestionB();
-
-var S1 = Console.ReadLine();
-var array1 = S1.Split(' ').Select(x => int.Parse(x)).ToList();
-
-int result = Enumerable.Range(1, array1[0]).Where(x => SumRange(x) == array1[1]).Count();
-
-Console.WriteLine($"{result}");
-
-int SumRange(int x)
-{
-    int sum = 0;
-    while (x > 0)
-    {
-        sum += x % 10;
-        x /= 10;
-    }
-    return sum;
-}
